bd9b506bad76caad9af126f60f06e5464dcb568b
Sruthi Sudhakar <sruthis@gatech.edu>
2019-10-26 02:06:15 -0400

    asdf

--- sendEmail.py
+++ sendEmail.py
@@ -1,11 +1,5 @@
 import smtplib, ssl
 
-# Python3 program for reverse geocoding.
-
-# importing necessary libraries
-import reverse_geocoder as rg
-import pprint
-
 def sendEmail(receiver_email, message):
     sender_email = "hostilearchitectureawareness@gmail.com"
     password = "hostilearch123"
@@ -19,16 +13,8 @@ def sendEmail(receiver_email, message):
     print ("success")
     return;
 
-def reverseGeocode(coordinates):
-    result = rg.search(coordinates)
-
-    # result is a list containing ordered dictionary.
-    pprint.pprint(result)
-
 def main():
     sendEmail("sruthisudhakar81@gmail.com", "yo")
-    coordinates =(28.613939, 77.209023)
-
-    reverseGeocode(coordinates)
+    reverseGeocode()
 
 if __name__ == "__main__": main()
