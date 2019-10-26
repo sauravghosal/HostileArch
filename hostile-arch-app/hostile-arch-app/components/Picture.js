73955ddb918650c2a86b65c5d0387daae47dece5
Tashfia Rahman <trahman@lawn-128-61-1-148.lawn.gatech.edu>
2019-10-26 02:27:58 -0400

    location in progress

--- hostile-arch-app/hostile-arch-app/components/Picture.js
+++ hostile-arch-app/hostile-arch-app/components/Picture.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
-import FontAwesome, {
-  SolidIcons,
-  RegularIcons,
-  BrandIcons,
-  parseIconFromClassName
-} from "react-native-fontawesome";
 
 function Picture(props) {
   return (
     <View>
       <Text>Click to upload picture</Text>
       {/* Figure out icons here */}
-      <FontAwesome icon={BrandIcons.github} />
     </View>
   );
 }
