14d350c268798b854c85e37bbb245629b4ad8ee5
Sruthi Sudhakar <sruthis@gatech.edu>
2019-10-26 13:23:54 -0400

    asdf

--- reverseGeoCode.py
+++ reverseGeoCode.py
@@ -3,48 +3,15 @@ import googlemaps
 from datetime import datetime
 
 
-def reverseGeocode():
-    # URL = "http://maps.googleapis.com/maps/api/geocode/json"
-
-    # # location given here
-    # location = "delhi technological university"
-
-    # # defining a params dict for the parameters to be sent to the API
-    # PARAMS = {'address':location}
-
-    # # sending get request and saving the response as response object
-    # r = requests.get(url = URL, params = PARAMS)
-
-    # # extracting data in json format
-    # data = r.json()
-
-    # print(data)
-
-    # # extracting latitude, longitude and formatted address
-    # # of the first matching location
-    # latitude = data['results'][0]['geometry']['location']['lat']
-    # longitude = data['results'][0]['geometry']['location']['lng']
-    # formatted_address = data['results'][0]['formatted_address']
-
-    # # printing the output
-    # print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
-    #       %(latitude, longitude,formatted_address))
+def reverseGeocode(coordinates):
     gmaps = googlemaps.Client(key="AIzaSyACXhXaxtZ3mw7-2d2vjIckekoE4lQbY48")
 
-    # Geocoding an address
-    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
     # Look up an address with reverse geocoding
-    reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-    # Request directions via public transit
-    now = datetime.now()
-    directions_result = gmaps.directions("Sydney Town Hall",
-                                         "Parramatta, NSW",
-                                         mode="transit",
-                                         departure_time=now)
+    result = gmaps.reverse_geocode(coordinates)
+    ans = result[0]["formatted_address"]
+    print(ans)
 
 def main():
-    reverseGeocode()
+    reverseGeocode((40.714224, -73.961452))
 
 if __name__ == "__main__": main()
--- sendEmail.py
+++ sendEmail.py
@@ -1,19 +1,75 @@
 import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image     import MIMEImage
+from email.header         import Header
+import base64
 
 def sendEmail(receiver_email, message):
+
     sender_email = "hostilearchitectureawareness@gmail.com"
     password = "hostilearch123"
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "99!!!!!!!"
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Create the body of the message (a plain-text and an HTML version).
+    #text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        <img src="cid:image1">
+        <p>99<br>
+           NINEINEEEEE<br>
+           WE LOVE BROOKLYN NINENINEEEE WATCH THIS PLEASE <a href="https://www.youtube.com/watch?v=zE5sEbEehNo">link</a>!!!!
+        </p>
+      </body>
+    </html>
+    """
+
+    # Encapsulate the plain and HTML versions of the message body in an
+    # 'alternative' part, so message agents can decide which they want to display.
+    msgAlternative = MIMEMultipart('alternative')
+    msg.attach(msgAlternative)
+
+    msgText = MIMEText('This is the alternative plain text message.')
+    msg.attach(msgText)
+
+    # We reference the image in the IMG SRC attribute by the ID we give it below
+    msgText = MIMEText(html, 'html')
+    msg.attach(msgText)
+
+    # This example assumes the image is in the current directory
+    fp = open('bench.jpg', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', '<image1>')
+    msg.attach(msgImage)
+
+
     # Create secure connection with server and send email
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(
-            sender_email, receiver_email, message
+            sender_email, receiver_email, msg.as_string()
         )
     print ("success")
     return;
+def decodeAndSaveLocally(encoded_string):
+    with open("bench.jpg", "wb") as fh:
+        fh.write(base64.decodebytes(encoded_string))
 
 def main():
-    sendEmail("sruthisudhakar81@gmail.com", "yo")
+    # with open("brooklyn-nine-nine.jpg", "rb") as image_file:
+    #     encoded_string = base64.b64encode(image_file.read())
+    decodeAndSaveLocally(encoded_string)
+    sendEmail("sruthisudhakar81@gmail.com", "asdf")
 
 if __name__ == "__main__": main()
