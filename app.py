9998a9656de19521d425164fa06c0123d9b6955e
Aman Jain <aman.jain@gatech.edu>
2019-10-26 13:58:21 -0400

    consolidation

--- app.py
+++ app.py
@@ -3,23 +3,32 @@ import smtplib, ssl
 from arcgis.gis import GIS
 from arcgis import geometry
 from arcgis import features
+import requests
+import googlemaps
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/poster", methods=['POST'])
+@app.route("/posting_email_info", methods=['POST'])
 def hello():
     msg_body = request.get_json()
     text = msg_body['text']
-    loc_x = float(msg_body['loc_x'])
-    loc_y = float(msg_body['loc_y'])
+    loc_x = float(msg_body['x'])
+    loc_y = float(msg_body['y'])
+    pic = msg_body['picture_base_64']
+    sender_email = msg_body['sender_email']
     tup = getRepresentative(loc_x, loc_y)
     if tup is None:
         return "Representative not found"
     else:
         state, district, name = tup
-        send = text + "\n:) it works!\nYour representative is: Representative " + name + " from " + state+ "'s district number " +  district + ".\n"
-        sendEmail("amanj120@gmail.com", send)
-        return "Success"
+        address = reverseGeocode((loc_y,loc_x))
+        if address is None:
+            return "Unable to find address using reverse geocoding"
+        else:
+            send = text + "\n:) it works!\nYour representative is: Representative " + name + " from " + state+ "'s district number " +  district + ".\nYour address is " + address
+            sendEmail("amanj120@gmail.com", send)
+            return "Success"
 
 @app.route("/")
 def first():
@@ -37,7 +46,6 @@ def getRepresentative(x, y):
         state = attributes['STATE_ABBR']
         district = attributes['CDFIPS']
         ret_tup = (state, district, name)
-        print(state + " " + district + " " + name)
         return ret_tup
 
 def sendEmail(receiver_email, message):
@@ -52,6 +60,13 @@ def sendEmail(receiver_email, message):
         )
     print ("success")
     return;
+
+def reverseGeocode(coordinates):
+    gmaps = googlemaps.Client(key="AIzaSyACXhXaxtZ3mw7-2d2vjIckekoE4lQbY48")
+    # Look up an address with reverse geocoding
+    result = gmaps.reverse_geocode(coordinates)
+    ans = result[0]["formatted_address"]
+    return ans
 
 if __name__ == '__main__':
     dist_id = 'cc6a869374434bee9fefad45e291b779'
--- getLocationInformation.py
+++
@@ -1,9 +0,0 @@
-#use esri for this
-
-from arcgis.gis import GIS
-dist_id = 'cc6a869374434bee9fefad45e291b779'
-gis = GIS()
-item = gis.content.get(dist_id)
-item
-#this item thingy has everything we need, I just need to know how to use it
-#and also how big is it? ASK Esri people tomorrow
