f04f497ab23d1cf7aaa03324f5d76d8e1aca3fb8
Tashfia Rahman <trahman@lawn-128-61-1-148.lawn.gatech.edu>
2019-10-26 16:39:36 -0400

    base64 working

--- hostile-arch-app/hostile-arch-app/components/Camera.js
+++ hostile-arch-app/hostile-arch-app/components/Camera.js
@@ -11,25 +11,35 @@ export default class Camera extends React.Component {
   };
 
   selectPicture = async() => {
-      await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
-          aspect: 1,
-          allowsEditing: true,
-          base64: true,
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        base64: true,
       });
-      this.setState({ image: uri});
-      if (!cancelled) this.setState({ image: uri});
-      console.log(this.state);
+  
+      console.log(result);
+  
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+      }
   }
 
   takePicture = async() => {
-      await Permissions.askAsync(Permissions.CAMERA);
-      const { cancelled, uri } = await ImagePicker.launchCameraAsync({
-          allowsEditing: false,
-          base64: true,
+    await Permissions.askAsync(Permissions.CAMERA);
+    let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        base64: true,
       });
-      this.setState({ image : uri});
-      console.log(this.state);
+  
+      console.log(result);
+  
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+      }
   };
 
   render() {
@@ -43,54 +53,4 @@ export default class Camera extends React.Component {
           </View>
       )
   }
-
-//   render() {
-//     let { image } = this.state;
-
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Button
-//           title="Pick an image from camera roll"
-//           onPress={this._pickImage}
-//         />
-//         {image &&
-//           <Image source={{ uri: image }} style={{ width: 200, height: 200 }} 
-//           />}
-//       </View>
-//     );
-//   }
-
-//   componentDidMount() {
-//     this.getPermissionAsync();
-//   }
-
-//   getPermissionAsync = async () => {
-//     if (Constants.platform.ios) {
-//       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-//       if (status !== 'granted') {
-//         alert('Sorry, we need camera roll permissions to make this work!');
-//       }
-//     }
-//   }
-
-//   _takePicture = async() => {
-//       let result = await ImagePicker.launchCameraAsync({
-//           allowsEditing: false,
-//       });
-//       this.setState({ image : uri });
-//   }
-
-//   _pickImage = async () => {
-//     let result = await ImagePicker.launchImageLibraryAsync({
-//       mediaTypes: ImagePicker.MediaTypeOptions.All,
-//       allowsEditing: true,
-//       aspect: [4, 3],
-//     });
-
-//     console.log(result);
-
-//     if (!result.cancelled) {
-//       this.setState({ image: result.uri });
-//     }
-//   };
 }
