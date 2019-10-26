62707db864e47e952df7f63b863b4d16a72ccb47
Saurav Ghosal <sghosal9@gatech.edu>
2019-10-26 02:26:19 -0400

    camera icon

--- hostile-arch-app/hostile-arch-app/components/Picture.js
+++ hostile-arch-app/hostile-arch-app/components/Picture.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Text, View } from "react-native";
-import FontAwesome, {
-  SolidIcons,
-  RegularIcons,
-  BrandIcons,
-  parseIconFromClassName
-} from "react-native-fontawesome";
+import { Text, View, StyleSheet } from "react-native";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+
+const styles = StyleSheet.create({
+  scene: {
+    backgroundColor: "#F9E8D5",
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+  card: {
+    backgroundColor: "#E6D5C3",
+    flex: 0.2,
+    flexDirection: "column",
+    marginTop: "20%" // Replace by 20
+  },
+  container: {
+    backgroundColor: "#FFFFFF",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center"
+  }
+});
 
 function Picture(props) {
   return (
-    <View>
-      <Text>Click to upload picture</Text>
+    <View style={styles.scene}>
       {/* Figure out icons here */}
-      <FontAwesome icon={BrandIcons.github} />
+      <FontAwesome name="camera" size={50} onp />
     </View>
   );
 }
